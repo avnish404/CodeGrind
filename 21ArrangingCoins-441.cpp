ef8136e9892b0deeee0699c91334dc86a8a33083
avnish404 <avnishshukla0404@gmail.com>
2025-04-29 01:53:39 +0530

    20thQuestion

---
+++ 21ArrangingCoins-441.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    public:
+        int arrangeCoins(int n) {
+           int i=1;
+    
+           while(n>=i)
+           {
+            n-=i;
+            i++;
+           }
+           return i-1;
+        }
+    };
+
+
+    /*
+    Example 1:
+
+
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+
+Input: n = 8
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3.
+    */
