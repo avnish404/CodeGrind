2828e792fd81adfffa125cc5aac25a0cef0a3ed3
avnish404 <avnishshukla0404@gmail.com>
2025-05-19 01:37:41 +0530

    44thQuestion(Binary Search)

---
+++ 44Sqrt(x).cpp
@@ -0,0 +1,74 @@
+
+int floorSqrt(long long int N)
+{
+    // Write your code here.
+   long long int s=0;
+   long long int e=N;
+   long long int mid=s+(e-s)/2;
+    long long int ans=1;
+    while(s<=e)
+    {
+        if((mid*mid) == N )
+        {
+            return mid;
+        }
+
+        else if((mid*mid)<N)
+        {
+            ans=mid;
+            s=mid+1;
+            
+        }
+        else
+        {
+             e=mid-1;
+           
+        }
+        mid=s+(e-s)/2;
+    }
+
+    return ans;
+}
+
+
+/*
+
+Problem statement
+You are given a positive integer ‘n’.
+
+
+
+Your task is to find and return its square root. If ‘n’ is not a perfect square, then return the floor value of sqrt(n).
+
+
+
+Example:
+Input: ‘n’ = 7
+
+Output: 2
+
+Explanation:
+The square root of the number 7 lies between 2 and 3, so the floor value is 2.
+
+
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1:
+6
+Sample Output 1:
+2
+Explanation of Sample Input 1:
+The square root of the given number 6 lies between 2 and 3, so the floor value is 2.
+Sample Input 2:
+100
+Sample Output 2:
+10
+Explanation of Sample Output 2:
+The square root of the given number 100 is 10.
+Expected Time Complexity:
+Try solving this in O(log(n)).
+Constraints:
+0 <= n <= 10 ^ 9
+
+Time Limit: 1 sec.
+
+*/
