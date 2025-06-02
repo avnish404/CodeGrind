14e10b0d65b4960158319aed8b0d98dcd2fa46a2
avnish404 <avnishshukla0404@gmail.com>
2025-06-02 22:58:15 +0530

    51stQuestion(Bubble Sort)

---
+++ 51BubbleSort.cpp
@@ -0,0 +1,38 @@
+void bubbleSort(vector<int>& arr, int n)
+{   
+    // Write your code here.
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+
+    }
+    
+}
+
+
+
+/*
+
+Sample Input 1:
+1
+5
+6 2 8 4 10
+Sample Output 1:
+2 4 6 8 10
+Sample Input 2:
+2
+2
+1 2
+4
+4 3 2 1
+Sample Output 2:
+1 2
+1 2 3 4
+
+*/
