a3e99e58973c031fe6ca28f8fd329196b459be9c
avnish404 <avnishshukla0404@gmail.com>
2025-06-02 23:20:27 +0530

    51st Question (Bubble sort optimised)

--- 51BubbleSort.cpp
+++ 51BubbleSort.cpp
@@ -1,6 +1,7 @@
 void bubbleSort(vector<int>& arr, int n)
 {   
     // Write your code here.
+    bool swapped=false;
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n-1-i;j++)
@@ -8,7 +9,13 @@ void bubbleSort(vector<int>& arr, int n)
             if(arr[j]>arr[j+1])
             {
                 swap(arr[j],arr[j+1]);
+                swapped=true;
             }
+        }
+
+        if(swapped==false)
+        {
+            break;
         }
 
     }
