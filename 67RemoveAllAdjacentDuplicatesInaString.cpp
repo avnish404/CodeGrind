9dc720ee1b316cabbbfb48db316a7fbefdd90784
avnish404 <avnishshukla0404@gmail.com>
2025-06-11 04:13:22 +0530

    67thQuestion(Strings and characters)

---
+++ 67RemoveAllAdjacentDuplicatesInaString.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+
+        //First approach unoptimised
+        // int i=0;
+        // while(i<s.length())
+        // {
+        //     if(s[i]==s[i+1])
+        //     {
+        //         s.erase(i,2);
+        //         i=0;
+        //     }
+        //     else
+        //     {
+        //         i++;
+        //     }
+        // }
+        // return s;
+
+
+        //Second approach optimised
+
+        string result="";
+
+        for(char c:s)
+        {
+            if(!result.empty() && result.back()==c)
+            {
+                result.pop_back();
+            }
+            else
+            {
+                result.push_back(c);
+            }
+        }
+        return result;
+    }
+};
+
+
+
+/*
+
+1047. Remove All Adjacent Duplicates In String
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abbaca"
+Output: "ca"
+Explanation: 
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+Example 2:
+
+Input: s = "azxxzy"
+Output: "ay"
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+
+*/
