f8ce680f5011e7f461f851954b137663a4d23aba
avnish404 <91267441+avnish404@users.noreply.github.com>
2025-08-03 01:52:21 +0530

    79th Question

---
+++ 79SayDigit.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void sayDigit(int n, string arr[])
+{
+    if(n==0)
+    {
+        return ;
+    }
+    int digit = n%10;
+    
+    sayDigit(n/10,arr);
+    
+    cout<<arr[digit]<<" ";
+}
+
+int main()
+{
+string arr[10] = {"Zero", "One", "Two", "Three", "Four", 
+                      "Five", "Six", "Seven", "Eight", "Nine"};
+    
+    int n;
+    cin>>n;
+    
+    cout<<endl<<endl;
+    
+    sayDigit(n,arr);
+    
+}
